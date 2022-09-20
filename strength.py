52e2cfd93890a711492f737ba623ff40618618d4
quant@alpacka <admin@alpacka.se>
2022-09-20 21:40:14 +0200

    Adds ma compound period as an argument, default 1, 3, 6 months compound ma

--- strength.py
+++ strength.py
@@ -27,15 +27,14 @@ def calc_normalized_average(df, period):
     norm_av = (new_df.Close.mean() - new_df.Close[0]) / new_df.Close[0];
     return norm_av
 
-def calc_compound_ma(df):
-    ma_sum = calc_normalized_average(df, DateOffset(months=12))
-    ma_sum += calc_normalized_average(df, DateOffset(months=6))
-    ma_sum += calc_normalized_average(df, DateOffset(months=3))
-    ma_sum += calc_normalized_average(df, DateOffset(months=1))
-    return ma_sum / 3
+def calc_compound_ma(df, ma_period):
+    ma_sum = 0
+    for period in ma_period:
+        ma_sum += calc_normalized_average(df, DateOffset(months=period))
+    return ma_sum / len(ma_period)
 
-def calc_stats(df):
-    compound_ma = round(calc_compound_ma(df), 2)
+def calc_stats(df, ma_period):
+    compound_ma = round(calc_compound_ma(df, ma_period), 2)
     ma200 = calc_average(df, "200B")
     above_ma200 = df[-1:].Close[0] > ma200
     ma50 = calc_average(df, "50B")
@@ -53,15 +52,19 @@ def create_report(ticker, name, asset_stats):
                     asset_stats.above_ma200,
                     asset_stats.ma50_gt_ma200)
 
-def get_stats(ticker):
+def get_ticker(ticker):
     ticker = yf.Ticker(ticker)
     df = ticker.history("1y")
-    stats = calc_stats(df)
-    return stats
+    return df
+
+def get_ma_period(ma_period_str):
+    periods = { "136": [1, 3, 6], "3612" : [3, 6, 12], "13612" : [1, 3, 6, 12] }
+    return periods[ma_period_str]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Calculates momentum strength of assets in portfolio")
-    parser.add_argument("-p", "--portfolio", dest="portfolio", type=str, required = True, help='portfolio file name')
+    parser.add_argument("-p", "--portfolio", dest="portfolio", type=str, required = True, help="portfolio file name")
+    parser.add_argument("-ma", dest="ma_period", type=str, choices=["136", "3612", "13612"], default="136", help="compound ma period")
     args = parser.parse_args()
 
     try:
@@ -70,7 +73,8 @@ if __name__ == "__main__":
 
         stats_per_ticker = {}
         for ticker in list:
-            stats = get_stats(ticker)
+            df = get_ticker(ticker)
+            stats = calc_stats(df, get_ma_period(args.ma_period))
             stats_per_ticker[ticker] = AssetData(ticker, list[ticker]["name"], stats)
 
         a = sorted(stats_per_ticker.items(), key=lambda item: item[1].stats.compound_ma, reverse=True)
