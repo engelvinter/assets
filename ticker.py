71db76e2f2fb35d55fa80168ae323167988552ae
quant@alpacka <admin@alpacka.se>
2023-01-18 21:08:32 +0100

    Adds ticker module to get start date of ticker

---
+++ ticker.py
@@ -0,0 +1,16 @@
+import argparse
+import yfinance as yf
+
+def get_history(symbol):
+    ticker = yf.Ticker(symbol)
+    df = ticker.history(period="max")
+    return df
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Displays information about stock ticker")
+    parser.add_argument("symbol", type=str, help="symbol of the ticker")
+    args = parser.parse_args()
+
+    df = get_history(args.symbol)
+    print("Symbol: {}".format(args.symbol))
+    print("Start: {}".format(df.head(1).index[0]))
