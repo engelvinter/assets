495cf31759eedf484970d7a2b9a8df25ab54ccca
quant@alpacka <admin@alpacka.se>
2023-06-27 10:19:59 +0200

    Add simulation of trend-follwoing 3612

--- simulate.py
+++ simulate.py
@@ -3,39 +3,75 @@ import yfinance as yf
 import yaml
 import pandas as pd
 
-def get_symbol_history(symbol, period):
+from dataclasses import dataclass
+
+@dataclass
+class Asset:
+    symbol: str
+    price: float
+    amount: float
+
+@dataclass
+class Portfolio:
+    cash: float
+    assets : list
+
+def get_symbol_prices(symbol, period):
     ticker = yf.Ticker(symbol)
     df = ticker.history(period)["Close"]
     df.name = symbol
     return df
 
-def get_portfolio_history(symbol_list):
+def get_portfolio_prices(symbol_list):
     data = []
     for ticker in symbol_list:
-        history = get_symbol_history(ticker, "max")
+        history = get_symbol_prices(ticker, "max")
         data.append(history)
 
-    # Using inner join to reduce index to only existing dates per asset
+    # Using inner join to reduce index to only existing dates for every asset
     df = pd.concat(data, join="inner", axis = 1)
-    return df
+    # Assure only business days are in the index
+    df = df.asfreq('B')
+    return df.dropna()
 
 def calc_momentum_score(portfolio_history):
-    df1 = portfolio_history.pct_change(30).dropna()
-    df3 = portfolio_history.pct_change(90).dropna()
-    df6 = portfolio_history.pct_change(180).dropna()
-    df12 = portfolio_history.pct_change(360).dropna()
+    df3 = portfolio_history.pct_change(90)
+    df6 = portfolio_history.pct_change(180)
+    df12 = portfolio_history.pct_change(360)
 
-    first_date = max(df1.index[0], df3.index[0], df6.index[0], df12.index[0])
-    last_date = min(df1.index[-1], df3.index[-1], df6.index[-1], df12.index[-1])
-    common_index = pd.date_range(first_date, last_date)
+    mom = (df3 + df6 + df12) / 3
 
-    cdf1 = df1.reindex(common_index)
-    cdf3 = df3.reindex(common_index)
-    cdf6 = df6.reindex(common_index)
-    cdf12 = df12.reindex(common_index)
+    return mom.dropna()
 
-    mom = cdf1 + cdf3 + cdf6 + cdf12
-    return mom
+def evaluate(portfolio, portfolio_history, scores):
+    max_asset_index = scores.nlargest(3).index
+    date = scores.name
+    closes = portfolio_history.loc[date]
+
+    # sell all
+    if len(portfolio.assets) > 0:
+        for asset in portfolio.assets:
+            portfolio.cash += asset.amount * closes[asset.symbol]
+        portfolio.assets.clear()
+
+    portfolio_value = portfolio.cash
+    invest = portfolio.cash / 3
+
+    # buy new
+    for asset in max_asset_index:
+        portfolio.cash -= invest
+
+        a = Asset(asset,
+                  closes[asset],
+                  invest / closes[asset])
+        portfolio.assets.append(a)
+
+    return [max_asset_index.to_list(), portfolio_value]
+
+def simulate(portfolio, portfolio_history, momentum_score):
+    monthly_score = momentum_score.groupby(pd.Grouper(freq='M')).tail(1)
+    tmp = monthly_score.apply(lambda scores: evaluate(portfolio, portfolio_history, scores), axis = 1)
+    return tmp
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simulates momentum strategy using given portfolio")
@@ -46,9 +82,14 @@ if __name__ == "__main__":
         f = open(args.portfolio)
         assets = yaml.safe_load(f)
 
-        df = get_portfolio_history(assets.keys())
+        df = get_portfolio_prices(assets.keys())
         mom = calc_momentum_score(df)
-        print(mom)
+
+        p = Portfolio(10000, [])
+        tmp = simulate(p, df, mom)
+
+        with open('simulate.txt', 'w') as f:
+            f.write(tmp.to_csv())
 
     except (FileNotFoundError) as e:
         print(e)
